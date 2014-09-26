4c107934776aa23f3f424c4709e561e078006081
342141332 <342141332@qq.com>
2014-09-26 13:02:38 +0800

    1

---
+++ projects/checkmate/server/core/java/src/com/gearbrother/mushroomWar/pojo/BattleItemSoilder.java
@@ -0,0 +1,8 @@
+package com.gearbrother.mushroomWar.pojo;
+
+import com.gearbrother.mushroomWar.rpc.annotation.RpcBeanPartTransportable;
+
+@RpcBeanPartTransportable
+public class BattleItemSoilder extends BattleItem {
+
+}
